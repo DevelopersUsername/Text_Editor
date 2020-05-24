93422e89330b9b640e2946361a32b806f323d764
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-05-24 16:36:40 +0300

    Add elements stage 2

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -2,6 +2,9 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
@@ -17,27 +20,48 @@ public class TextEditor extends JFrame {
 
         getContentPane().setLayout(new FlowLayout());
 
-        JTextArea filenameField = new JTextArea(1, 10);
-        filenameField.setName("FilenameField");
-        filenameField.setBounds(10, 5, 5, 5);
-        add(filenameField);
-
+        JTextField fileNameField = new JTextField();
         JButton saveButton = new JButton("Save");
+        JButton loadButton = new JButton("Load");
+        JTextArea textArea = new JTextArea(20, 20);
+        JScrollPane scrollableTextArea = new JScrollPane(textArea);
+
+        fileNameField.setName("FilenameField");
+        fileNameField.setColumns(7);
+        add(fileNameField);
+
         saveButton.setName("SaveButton");
         saveButton.setBounds(100, 70, 100, 30);
+        saveButton.addActionListener(actionEvent -> whiteFile(fileNameField.getText(), textArea.getText()));
         add(saveButton);
 
-        JButton loadButton = new JButton("Load");
         loadButton.setName("LoadButton");
         loadButton.setBounds(100, 70, 100, 30);
+        loadButton.addActionListener(actionEvent -> textArea.setText(readFile(fileNameField.getText())));
         add(loadButton);
 
-        JTextArea textArea = new JTextArea(20, 20);
         textArea.setName("TextArea");
         textArea.setBounds(40, 20,150, 20);
 
-        JScrollPane scrollableTextArea = new JScrollPane(textArea);
         scrollableTextArea.setName("ScrollPane");
         getContentPane().add(scrollableTextArea);
+    }
+
+    String readFile(String pathToFile) {
+        try {
+            return Files.readString (Paths.get ("./Text Editor/task/" + pathToFile));
+        } catch(IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+            return "";
+        }
+    }
+
+    void whiteFile(String pathToFile, String text) {
+
+        try (OutputStream outputStream = new FileOutputStream("./Text Editor/task/" + pathToFile, false)) {
+            outputStream.write(text.getBytes());
+        } catch(IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+        }
     }
 }
